967121e46136bb6af1e98bcb2f8070c65719a778
Safa Ariguib <safaariguib284@gmail.com>
2026-04-19 18:05:44 +0100

    Clean up update method in AdminNotificationObserver
    Remove commented-out code regarding admin notification functionality.

--- src/main/java/pattern/observer/AdminNotificationObserver.java
+++ src/main/java/pattern/observer/AdminNotificationObserver.java
@@ -10,10 +10,7 @@ public class AdminNotificationObserver implements ReservationObserver {
  
  @Override
  public void update(Reservation reservation, String evenementType) {
-     // NOUVELLE FONCTIONNALITÉ : notifier l'admin
      System.out.println("[Observer] Notification à l'administrateur: " + 
                         "Réservation " + reservation.getId() + " - " + evenementType);
-     
-     // Ici vous pouvez ajouter un email à l'admin ou un log
  }
 }
